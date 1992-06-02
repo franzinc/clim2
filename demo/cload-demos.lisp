e418553ed4ea0a2e9cf33b694d9bc4459976626b
cer <cer>
1992-06-02 08:26:29 +0000

    Initial revision

---
+++ demo/cload-demos.lisp
@@ -0,0 +1,51 @@
+;; -*- mode: common-lisp; package: user -*-
+;;
+;;				-[]-
+;; 
+;; copyright (c) 1985, 1986 Franz Inc, Alameda, CA  All rights reserved.
+;; copyright (c) 1986-1992 Franz Inc, Berkeley, CA  All rights reserved.
+;;
+;; The software, data and information contained herein are proprietary
+;; to, and comprise valuable trade secrets of, Franz, Inc.  They are
+;; given in confidence by Franz, Inc. pursuant to a written license
+;; agreement, and may be stored and used only in accordance with the terms
+;; of such license.
+;;
+;; Restricted Rights Legend
+;; ------------------------
+;; Use, duplication, and disclosure of the software, data and information
+;; contained herein by any agency, department or entity of the U.S.
+;; Government are subject to restrictions of Restricted Rights for
+;; Commercial Software developed at private expense as specified in FAR
+;; 52.227-19 or DOD FAR Supplement 252.227-7013 (c) (1) (ii), as
+;; applicable.
+;;
+;; $fiHeader: cload-demos.cl,v 1.1 91/07/25 19:03:22 cer Exp $
+
+(defvar *demo-files* 
+    '("packages"
+      "aaai-demo-driver"
+      "cad-demo"
+      "thinkadot" 
+      "graphics-demos" 
+      "address-book" 
+      "listener" 
+      "navfun"
+      "navdata"
+      "puzzle"))
+
+(defun compile-and-load-demos (&key forcep 
+				    (directory
+				     (make-pathname 
+				      :directory (pathname-directory
+						  #.(truename 
+						     excl::*source-pathname*)))))
+  (mapcar #'(lambda (name)
+	      (let ((name (merge-pathnames
+			   directory
+			   name)))
+		(if forcep
+		    (compile-file name)
+		  (excl::compile-file-if-needed name))
+		(load name)))
+	  *demo-files*))
