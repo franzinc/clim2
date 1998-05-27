84dae6e65d9be4cbc88517f0645c67d3e6c2772a
layer <layer>
1998-05-27 20:30:42 +0000

    2.2.1.18

--- makedist.sh
+++ makedist.sh
@@ -2,13 +2,18 @@
 set -xe
 
 make clean
-make RECORD_XREF_INFO=nil RECORD_SOURCE_FILE_INFO=nil
+make RECORD_XREF_INFO=nil RECORD_SOURCE_FILE_INFO=nil CL=../src/lisp
 
 rm -fr dist temp.zip temp.exe
 mkdir dist dist/code dist/src dist/src/clim dist/src/clim/demo
 cp -p climnt.fasl climps.fasl climdemo.fasl climg.fasl dist/code
 cp -p readme.win32 dist/clim221.txt
 cp -p demo/*.lisp dist/src/clim/demo
+cp -p buildclim.cl dist
+
+# next 2 for beta only:
+cp -p ../src/winapi/winapi.fasl dist/code
+cp -p ../src/winapi/winapi-dev.fasl dist/code
 
 /winzip/winzip32 -min -a -r -sclim.me -ex temp.zip dist
 /winzipse/wzipse32 temp.zip -y -3 -win32 -le \
