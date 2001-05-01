6d8a5bc16ec05c2d2f4660f1c6d7bc6e93697f68
duane <duane>
2001-05-01 21:27:18 +0000

    see 2.2.2.21.16 changelog entry for Tue May  1 15:25:21 PDT 2001

--- utils/packages.lisp
+++ utils/packages.lisp
@@ -16,7 +16,7 @@
 ;; Commercial Software developed at private expense as specified in
 ;; DOD FAR Supplement 52.227-7013 (c) (1) (ii), as applicable.
 ;;
-;; $Id: packages.lisp,v 1.106.2.6 2001/04/24 19:53:22 layer Exp $
+;; $Id: packages.lisp,v 1.106.2.7 2001/05/01 21:27:18 duane Exp $
 
 (in-package :common-lisp-user)
 
@@ -3308,7 +3308,7 @@
 (cl:defvar *clim-major-version* 2)
 (cl:defvar *clim-minor-version* 2)
 (cl:defvar *clim-generation-version* 2)
-(cl:defvar *clim-build-version* "21.15")
+(cl:defvar *clim-build-version* "21.16")
 
 (cl:defparameter *clim-version*
     (cl:format () "~d.~d.~d.~a"
