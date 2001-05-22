61612dd71bad13afa9d6061c9be46a10012ee1a0
layer <layer>
2001-05-22 23:06:24 +0000

    2.2.2.21.14.3

--- utils/packages.lisp
+++ utils/packages.lisp
@@ -16,7 +16,7 @@
 ;; Commercial Software developed at private expense as specified in
 ;; DOD FAR Supplement 52.227-7013 (c) (1) (ii), as applicable.
 ;;
-;; $Id: packages.lisp,v 1.106.2.5.2.2 2001/05/17 17:32:10 layer Exp $
+;; $Id: packages.lisp,v 1.106.2.5.2.3 2001/05/22 23:06:24 layer Exp $
 
 (in-package :common-lisp-user)
 
@@ -3308,7 +3308,7 @@
 (cl:defvar *clim-major-version* 2)
 (cl:defvar *clim-minor-version* 2)
 (cl:defvar *clim-generation-version* 2)
-(cl:defvar *clim-build-version* "21.14.2")
+(cl:defvar *clim-build-version* "21.14.3")
 
 (cl:defparameter *clim-version*
     (cl:format () "~d.~d.~d.~a"
