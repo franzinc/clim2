9814bfd04a2be8c612e6969a52f57a4259dbad83
layer <layer>
2002-06-04 22:49:56 +0000

    2002-06-04    <layer@HOBART>

--- aclpc/acl-class.lisp
+++ aclpc/acl-class.lisp
@@ -16,7 +16,7 @@
 ;; Commercial Software developed at private expense as specified in
 ;; DOD FAR Supplement 52.227-7013 (c) (1) (ii), as applicable.
 ;;
-;; $Id: acl-class.lisp,v 1.14.24.2.24.3 2002/05/23 15:57:47 layer Exp $
+;; $Id: acl-class.lisp,v 1.14.24.2.24.4 2002/06/04 22:49:56 layer Exp $
 
 #|****************************************************************************
 *                                                                            *
@@ -1399,6 +1399,21 @@
 	     (win:DispatchMessage msg)))
       (when nonblocking (return t))
       )))
+
+(defvar *current-window*)
+(defvar *dc-initialized* nil)
+
+;;; Stock objects
+(defvar *null-pen*)
+(defvar *black-pen*)
+(defvar *ltgray-pen*)
+(defvar *null-brush*)
+(defvar *black-brush*)
+(defvar *ltgray-brush*)
+(defvar *blank-image*)
+(defvar *ltgray-image*)
+
+(defvar *background-brush* nil)
 
 ;;; spr25546
 ;;; Re-initialize a number of state-variables/pointers associated
